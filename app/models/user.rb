2e85a21044664e0a3ded5bb1a1ddad14aa024dbb
Lorena Andrea Carcamo Herrera <lorenacarcamo6@gmail.com>
2019-03-11 15:13:00 -0300

    Implementación de OmniAuth

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
-
+  devise :omniauthable, :omniauth_providers => [:facebook]
   has_many :orders
   has_many :details, through: :orders
 
@@ -16,6 +16,17 @@ class User < ApplicationRecord
   enum role: %i[user vendor admin ghest]
   enum phone_type: %i[celular fijo]
 
+  #OmniAuth de User
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+      user.name = auth.info.name
+      user.image= auth.info.image
+    end
+  end
+
+  #Carrito de Compra del usuario
   def cart
     orders.where(paided: false)
   end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,11 @@ Rails.application.routes.draw do
   resources :banners 
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  devise_for :users
+  devise_for :users,
+  controllers: {
+    omniauth_callbacks: 'users/omniauth_callbacks'
+  }
+  
   resources :profiles, only: [:index] do
     collection do
       get 'address', to: 'profiles#address', as: 'address'
